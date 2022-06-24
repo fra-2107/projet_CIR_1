985dd60fae7ac4335c72c38c1dc3b9472d793baf
fra-2107 <timchimienti@gmail.com>
2022-06-24 14:20:58 +0200

    css

--- Code/WEB/script.js
+++ Code/WEB/script.js
@@ -1,4 +1,8 @@
 let mercury
+let start
+let earth
+let sun
+
 
 async function request(){
     const reponse = await fetch('/mercury.json');
@@ -6,11 +10,17 @@ async function request(){
     mercury = fichier['mercury-euler'];
 }
 async function setup(){
-    createCanvas(windowWidth, windowHeight, WEBGL);
+    createCanvas(windowWidth, windowHeight);
     background(0);
-    let start = false;
+    start = false;
     await request();
     start = true;
+    // affichage du soleil
+    sun = createImg(
+        '/assets/soleil.png'
+    );
+    sun.position(700, 300);
+    sun.size(75,75);
 }
 
 function preload(){
@@ -20,7 +30,15 @@ function preload(){
 function draw(){
     if(start)
     {
-        
+        //affichage de la terre
+        earth = createImg(
+            '/assets/earth.png'
+        );
+        earth.position(300, 10);
+        earth.size(50, 50);  
     }
 }
 
+function mouseClicked() {
+    earth.remove();
+}
