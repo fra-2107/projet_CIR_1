a47ab9be632e8b372886f20e071ec4e3c5455d6d
kikball9 <76593965+kikball9@users.noreply.github.com>
2022-06-29 00:53:03 +0200

    planet.c commenté

--- Code/C/Fonctions/main.c
+++ Code/C/Fonctions/main.c
@@ -3,11 +3,27 @@
 int main(){
 
     // vectorTest();
-    Planet *planetList = InitPlanet("Data/infoAstre.json");    
-    
-    // DeltaConservationEnergie(planetList, 0, NB_REPERE-1, "Euler");
-    // DeltaConservationEnergie(planetList, 0, NB_REPERE-1, "EulerAsy");
-    // DeltaConservationEnergie(planetList, 0, NB_REPERE-1, "RK2");
+    Planet *planetList = InitPlanet("Data/infoAstre.json");
+
+    char methodeList[3][10] = {"Euler", "EulerAsy", "RK2"};
+
+    for(int k=0; k<3; k++){
+
+        char path[30];
+        sprintf(path, "../Data/%s.json", methodeList[k]);
+
+        for(int i=0; i<NB_ASTRE; i++){
+
+            planetList[i].trajectoire = malloc(NB_REPERE * sizeof(Point));
+            planetList[i].trajectoire[0] = firstPoint(planetList[i]);
+
+            planetList[i] = ChooseMethode(methodeList[k], planetList[i], NB_REPERE, PAS_MERCURE);   
+        }
+
+        writeForMethode(planetList, methodeList[k], path);
+
+        DeltaConservationEnergie(planetList, 0, NB_REPERE-1, methodeList[k]);
+    }    
 
     return 0;
 }
